81a8c3e74b9792203ceb2fad943db40dd0a28be1
TacoC02 <raix1034@gmail.com>
2026-02-14 17:07:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+﻿const messages = [
+    "Segura?",
+    "Realmente Segura?",
+    "por nueve mesesitos",
+    "Pero piensalo",
+    "Tu quieres",
+    "Pero piensatelo un momento",
+    "No pienses en los pañales",
+    "9 mesesitos, ojo",
+    "anda, anda",
+    "9 mesesitos sin eso"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
